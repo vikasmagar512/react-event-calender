54d062c17f273cfd6e121deb84002c73ab851310
vikasmagar512@gmail.com <vikasmagar512@gmail.com>
2020-01-01 01:26:22 +0530

    Updates

---
+++ precache-manifest.4404eb4464c9cc8036533e334ae072ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c15297c134b6c4fd347e110193750c6",
+    "url": "/react-event-calender/index.html"
+  },
+  {
+    "revision": "64f3332581e6f4286ade",
+    "url": "/react-event-calender/static/css/main.ff976e7f.chunk.css"
+  },
+  {
+    "revision": "efdb186a42f878b47d52",
+    "url": "/react-event-calender/static/js/2.332453ac.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-event-calender/static/js/2.332453ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "64f3332581e6f4286ade",
+    "url": "/react-event-calender/static/js/main.fe55227b.chunk.js"
+  },
+  {
+    "revision": "1194150f8197b6abc3a0",
+    "url": "/react-event-calender/static/js/runtime-main.277843f1.js"
+  }
+]);
